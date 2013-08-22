0c53b5a3e3f06b644f672c8db8f06efd797c35de
Deyin Xu <dxu@cloudswitch.com>
2013-08-22 16:07:49 -0400

    add column header to table data file

--- acs_parser/src/breakdata.py
+++ acs_parser/src/breakdata.py
@@ -4,7 +4,7 @@ import csv
 import os
 import re
     
-def createTableBlockDataFile(srcDir, fname, start, length) :
+def createTableBlockDataFile(srcDir, fname, start, length, columns=None) :
     theT = []
     fullpath=os.path.join(srcDir, fname + ".txt")
     with open(fullpath) as csvfile:
@@ -37,6 +37,52 @@ def main() :
         fname = "e20115ma" + fname +"000"
         ##the source file count field index starting from 1
         createTableBlockDataFile(SRC_DIR, fname, int(pos[1])-1, int(pos[2]))
+           
+def main1() : 
+    ##SRC_DIR = "/Users/dxu/Mapster/Massachusetts_Tracts_Block_Groups_Only"
+    SRC_DIR = "/home/dxu/mac_work"
+    fin = open('output/tableList.txt', 'r')
+    allTabDict = {}
+    tableNames = []
+    columns = []
+    
+    for line in fin:
+        ##process data block for this table
+        if not line :
+            continue;
+        
+        if line.startwith('    ') :
+            columns.append(line.strip(' '))
+        else :
+            ##new table name found
+            ##1. check old tableNames and columns first
+            if len(tableNames) != 0 and len(columns) != 0 :
+                for tn in tableNames :
+                    allTabDict[tn] = columns
+                ##2. reset the tableNames and columns  
+                tableNames = []
+                columns = []
+           
+            ##add new name to tableNames[]
+            tableNames.append(line)
+   
+    ## process the last table 
+    if len(tableNames) != 0 and len(columns) != 0 : 
+        for tn in tableNames : 
+            allTabDict[tn] = columns
+            
+    tabs= allTabDict.items()
+    
+    for tItem in tabs :
+        tt = tItem[0];   ##key=tablename, value = column []
+        pos = re.findall(r'\d+', tt);
+        if len (pos) == 0 :
+            continue
+        ## file name: e20115ma0002000, m20115ma0113000
+        fname = str(pos[0]).zfill(4)
+        fname = "e20115ma" + fname +"000"
+        ##the source file count field index starting from 1
+        createTableBlockDataFile(SRC_DIR, fname, int(pos[1])-1, int(pos[2]), tItem[1])
         
         
 if  __name__ =='__main__':main()
