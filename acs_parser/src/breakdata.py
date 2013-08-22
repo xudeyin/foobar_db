f67175353a205c47f8396d9b386bc0aceb1d3ae2
xudeyin <xudeyin@gmail.com>
2013-08-21 23:39:48 -0400

    add breakdata.py - this script takes the tables.txt output from sequence.py and the raw census file under Massachusetts_Tracts_Block_Groups_Only and break them into different data set files based on the table sequence

--- acs_parser/src/breakdata.py
+++ acs_parser/src/breakdata.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python
 
 import csv
-import string
-
-
-fin = open('output/tables.txt', 'r')
-
-for line in fin:
-    ##process data block for this table
-    if len(line.strip([' '])) == 0:
-        continue;
-    pos = [int(s) for s in line.split() if s.isdigit()]
-    ## file name: e20115ma0002000, m20115ma0113000
+import os
+import re
     
+def createTableBlockDataFile(srcDir, fname, start, length) :
+    theT = []
+    fullpath=os.path.join(srcDir, fname + ".txt")
+    with open(fullpath) as csvfile:
+        lreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for srcLine in lreader :
+            newLine = [srcLine[5]] + srcLine[start:start + length]
+            theT.append(newLine)
+   
+    fullpath = os.path.join(srcDir, fname + "_" + str(start+1) + "_" + str(length) + ".csv")
+    with open(fullpath, 'w')  as csvfile:
+        tWriter = csv.writer(csvfile, delimiter=',')
+        for newLine in theT:
+            tWriter.writerow(newLine)
+    return
+           
+def main() : 
+    SRC_DIR = "/Users/dxu/Mapster/Massachusetts_Tracts_Block_Groups_Only"
+
+    fin = open('output/tables.txt', 'r')
+
+    for line in fin:
+        ##process data block for this table
+        if not line :
+            continue;
+        pos = re.findall(r'\d+', line);
+        if len (pos) == 0 :
+            continue
+        ## file name: e20115ma0002000, m20115ma0113000
+        fname = str(pos[0]).zfill(4)
+        fname = "e20115ma" + fname +"000"
+        ##the source file count field index starting from 1
+        createTableBlockDataFile(SRC_DIR, fname, int(pos[1])-1, int(pos[2]))
+        
+        
+if  __name__ =='__main__':main()
