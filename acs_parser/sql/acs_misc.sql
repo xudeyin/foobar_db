9bbd3c0eb1a85b28935bb05ed45a8ecdeb572107
xudeyin <xudeyin@gmail.com>
2013-08-23 00:39:57 -0400

    Generate schema files

---
+++ acs_parser/sql/acs_misc.sql
@@ -0,0 +1,14 @@
+create table acs_dataset (
+    id          INT4 NOT NULL PRIMARY KEY,
+    table_name  VARCHAR (64) NOT NULL,
+    description VARCHAR (256)
+);
+
+create table acs_dataset_columns (
+    id          INT4 NOT NULL,
+    column_name VARCHAR (64) NOT NULL,
+    description VARCHAR (256),
+
+    PRIMARY KEY( id, column_name )
+    FOREIGN KEY( id ) REFERENCES acs_dataset
+);
--- acs_parser/src/breakdata.py
+++ acs_parser/src/breakdata.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python
 
+
 import csv
 import os
 import re
+from collections import Counter
+
+##
+## fschema: schema file
+## fdataset = acs_dataset data file
+## fcolumns = acs_dataset_columns
+##
+def createTableSchemaFile(tabIdx, fname, tItem, fschema, wdataset, wcolumns) : 
+    cCounter = Counter()
+    wdataset.writerow([tabIdx, fname, tItem[0].strip('\n')]) 
+    columns = tItem[1]
+    
+    fschema.write ("CREATE TABLE " + fname + " (\n")
+    
+    fschema.write("    " + columns[0].ljust(64) + " INT4 NOT NULL,\n")
+    for c in columns[1:] :
+        cName = c[:30]
+        cCounter.update([cName])
+        appendStr=str(cCounter[cName]-1).zfill(2);
+        fschema.write("    " + (cName + appendStr).ljust(32) + " INT4 NOT NULL DEFAULT 0,\n")
+        wcolumns.writerow([tabIdx, cName + appendStr, c])
+    fschema.write("    PRIMARY KEY (region_id)\n")
+    fschema.write(");\n\n")
     
 def createTableBlockDataFile(srcDir, fname, start, length, columns=None) :
     theT = []
@@ -16,6 +40,8 @@ def createTableBlockDataFile(srcDir, fname, start, length, columns=None) :
     fullpath = os.path.join(srcDir, fname + "_" + str(start+1) + "_" + str(length) + ".csv")
     with open(fullpath, 'w')  as csvfile:
         tWriter = csv.writer(csvfile, delimiter=',')
+        if columns :
+            tWriter.writerow(columns)
         for newLine in theT:
             tWriter.writerow(newLine)
     return
@@ -39,29 +65,30 @@ def main() :
         createTableBlockDataFile(SRC_DIR, fname, int(pos[1])-1, int(pos[2]))
            
 def main1() : 
-    ##SRC_DIR = "/Users/dxu/Mapster/Massachusetts_Tracts_Block_Groups_Only"
-    SRC_DIR = "/home/dxu/mac_work"
+    SRC_DIR = "/Users/dxu/Mapster/Massachusetts_Tracts_Block_Groups_Only"
+    ##SRC_DIR = "/home/dxu/mac_work"
     fin = open('output/tableList.txt', 'r')
     allTabDict = {}
     tableNames = []
-    columns = []
+    columns = ['region_id']
     
     for line in fin:
         ##process data block for this table
         if not line :
             continue;
         
-        if line.startwith('    ') :
-            columns.append(line.strip(' '))
+        if line.startswith('    ') :
+            columns.append(line.strip(' ').strip('\n'))
         else :
             ##new table name found
             ##1. check old tableNames and columns first
-            if len(tableNames) != 0 and len(columns) != 0 :
+            ## Note the columns[] already has at least 1 element : region_id
+            if len(tableNames) != 0 and len(columns) >1 :
                 for tn in tableNames :
                     allTabDict[tn] = columns
                 ##2. reset the tableNames and columns  
                 tableNames = []
-                columns = []
+                columns = ['region_id']
            
             ##add new name to tableNames[]
             tableNames.append(line)
@@ -70,7 +97,17 @@ def main1() :
     if len(tableNames) != 0 and len(columns) != 0 : 
         for tn in tableNames : 
             allTabDict[tn] = columns
-            
+
+    ##open schema file
+    fschema = open("sql/create_acs_tables.sql", "w")
+    ##open data file for acs_dataset
+    f_acs_dataset = open("sql/acs_dataset.csv", "w")
+    writer1 = csv.writer(f_acs_dataset, delimiter=',')
+    #open data file for acs_dataset_columns
+    f_acs_dataset_columns = open("sql/acs_dataset_columns.csv", "w")
+    writer2 = csv.writer(f_acs_dataset_columns, delimiter=',')
+    
+    tableIdx = 0        
     tabs= allTabDict.items()
     
     for tItem in tabs :
@@ -81,8 +118,20 @@ def main1() :
         ## file name: e20115ma0002000, m20115ma0113000
         fname = str(pos[0]).zfill(4)
         fname = "e20115ma" + fname +"000"
+        print "process file " + fname
+        
+        ## generate schema file and assistent table data files
+        createTableSchemaFile(tableIdx, fname+"_"+pos[1]+"_"+pos[2], tItem, fschema, writer1, writer2) 
+        
         ##the source file count field index starting from 1
         createTableBlockDataFile(SRC_DIR, fname, int(pos[1])-1, int(pos[2]), tItem[1])
         
+        tableIdx += 1
         
-if  __name__ =='__main__':main()
+    fschema.close()
+    f_acs_dataset.close()
+    f_acs_dataset_columns.close()
+    
+    print "Done!"
+        
+if  __name__ =='__main__':main1()
