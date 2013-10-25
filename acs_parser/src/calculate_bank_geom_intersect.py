b8d81aa764515c36da7b7bc293ea0048eca90a1d
Deyin Xu <dxu@cloudswitch.com>
2013-10-25 10:46:29 -0400

    Update the calculate intersection python script

--- acs_parser/src/calculate_bank_geom_intersect.py
+++ acs_parser/src/calculate_bank_geom_intersect.py
@@ -1,73 +1,54 @@
 #!/usr/bin/python
 
-# this script calculates the tract areas 
+# this script calculates the intersctions of circular buffers with various radius for each bank location and 
+# the tract or block groups defined in the TIGER file.
+# The calculation is based on SRID 26986 (for Massachusetts mainland)
 import psycopg2
 import time
 import csv
+import argparse
 
-# SRID 26986 is MA mainland
-# method 1: calculate the intersections using ST_DWithin() function.  Need to
-# transform the geom into SRID 26986 (or any other SCS that uses meter as the
-# unit.
-#
-# This method is reasonably fast
-def getIntersections(cur, point, radius) :
-    qstr='select geoid, name from tl_2011_25_bg where ST_DWithin(%s, ST_Transform(geom, 26986), %s)=True order by geoid'
+def getCurrentTime() :
+    return int(round(time.time() * 1000))
 
-    cur.execute(qstr, (point, radius))
-    cnt = 0
-    for t in cur.fetchall() :
-        print "    count"+str(cnt) + ":tract=" + str(t)
-        cnt += 1
-
-# spheroid_str='SPHEROID["GRS 1980",6378137,298.257222101]'
-# this is the spheroid for SRID 4269.  (the one used in the database)
-# method 2: calculate the intersections using ST_Distance_Spheroid() and the
-# above spheroid 
-# (ST_Distance_Sphere() can also be used, with less accuracy.
-#
-# this method is EXTREMELY slow!!!
-def getIntersections2(cur, point, radius) :
-    spheroid_str='SPHEROID["GRS 1980",6378137,298.257222101]'
-
-    qstr='select geoid, name from tl_2011_25_bg where ST_Distance_Spheroid(%s,geom,%s) < %s order by geoid'
-    cur.execute(qstr, (point, spheroid_str, radius))
-    cnt = 0
-    for t in cur.fetchall() :
-        print "    count"+str(cnt) + ":tract=" + str(t)
-        cnt += 1
-
-
-def getTractArea(cur) :
+def getTigerFileType(sType) :
+    if sType == 'tract' :
+	return 1
+    elif sType == 'bg' :
+        return 2
+    return -1
+        
+def getTractArea(cur, t) :
     tractDict={}
 
-    cur.execute("select geoid, ST_Area(ST_Transform(geom, 26986)) from tl_2011_25_bg")
+    cur.execute("select geoid, ST_Area(geom26986) from tl_2011_25_26986 where type=%s", (getTigerFileType(t),))
 
     for row in cur.fetchall() :
         tractDict[row[0]]=row[1];
 
     return tractDict
 
-def getIntersection3(cur, writer, bank_id, circle, c_area, srid, radius, tractDict) :
+def getIntersection3(cur, writer, bank_id, circle, c_area, srid, radius, t, tractDict) :
     qstr=''' select  
                 t.geoid, 
-                ST_Area(ST_Intersection(%s, t.tgeom)) as a_intersect 
+                ST_Area(ST_Intersection(%s, t.geom26986)) as a_intersect 
             from 
-                (select geoid, ST_Transform(geom, %s) as tgeom from tl_2011_25_bg) as t 
+                tl_2011_25_26986 t
             where 
-                ST_Intersects(%s, t.tgeom) '''
+                ST_Intersects(%s, t.geom26986)  and t.type = %s'''
 
-    cur.execute(qstr, (circle, srid, circle))
+    cur.execute(qstr, (circle, circle, getTigerFileType(t)))
    
     for t in cur.fetchall() :
         geoid=t[0]
         tractArea=tractDict[geoid]
         writer.writerow([bank_id, radius, geoid, c_area, tractArea, t[1], t[1]/tractArea*100])
 
-def calculateOneRadius(conn, srid, radius, tractDict) :
+def calculateOneRadius(conn, srid, radius, sType, tractDict) :
     cur1=conn.cursor()
     cur2=conn.cursor()
-   
+  
+    t1 = getCurrentTime(); 
     cur1.execute('''SELECT 
                            id, 
                            ST_Buffer(ST_Transform(geom, %s),%s),
@@ -75,11 +56,14 @@ def calculateOneRadius(conn, srid, radius, tractDict) :
                     FROM
                            bank_branch order by id''', (srid, radius, srid, radius))
 
-    with open('intersect_bg_' + str(radius) + '.csv', 'wb') as csvfile :
+##    print "    T1=" + str(getCurrentTime()-t1)
+    with open('intersect_' + sType + '_' + str(radius) + '.csv', 'wb') as csvfile :
         mywriter=csv.writer(csvfile) 
 
         for row in cur1.fetchall():
-            getIntersection3(cur2, mywriter, row[0], row[1], row[2], srid, radius, tractDict)
+            t1 = getCurrentTime(); 
+            getIntersection3(cur2, mywriter, row[0], row[1], row[2], srid, radius, sType, tractDict)
+##            print "        T2=" + str(getCurrentTime()-t1)
 
     cur1.close()
     cur2.close()
@@ -87,20 +71,29 @@ def calculateOneRadius(conn, srid, radius, tractDict) :
         
 def main() :
 
+    parser = argparse.ArgumentParser(description='Bank location buffer and tract/bg intersection calculation based on SRID 26986')
+    parser.add_argument('start_radius',  type=int,  help='Starting radius')
+    parser.add_argument('end_radius', type=int,  help='End radius')
+    parser.add_argument('step', type=int, help='Step')
+    parser.add_argument('type', choices=['tract', 'bg'],  help='process tract or block group')
+    args = vars(parser.parse_args())
+
+    print args
+
     current_time_milli= lambda: int(round(time.time() * 1000))
 
     conn = psycopg2.connect(database='mapster_db', user='mapster')
     
     cur1 = conn.cursor()
 
-    tractDict = getTractArea(cur1)
+    tractDict = getTractArea(cur1, args['type'])
 
     cur1.close()
 
-    for radius in xrange(100, 2001, 100) :
+    for radius in xrange(args['start_radius'], args['end_radius'] + 1, args['step']) :
         startT=current_time_milli()
         print "Processing radius " + str(radius) +"m. Start Time: " + str(startT)
-        calculateOneRadius(conn, 26986, radius, tractDict)
+        calculateOneRadius(conn, 26986, radius,  args['type'], tractDict)
         endT=current_time_milli()
         print "End Time: " + str(endT)
         print "Duration: " + str(endT-startT)
